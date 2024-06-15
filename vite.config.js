16035b1da5c82ee3894a14fc145edcefe971c938
Lman7154 <4138912@myuwc.ac.za>
2024-06-15 19:22:47 +0200

    routing configuration :)

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,7 @@ export default defineConfig({
     },
   },
 });
+
+//For each entry point, we use resolve(__dirname, 'path/to/file.html') to specify the path to the respective HTML file relative to the project root.
+
+//With this configuration, Vite will include all three HTML files (index.html, cv.html, and projects.html) in the build process, resulting in separate bundles for each entry point.
