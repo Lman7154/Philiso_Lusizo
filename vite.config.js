617771854104556179f6fa07934a36f72d715796
Lman7154 <4138912@myuwc.ac.za>
2024-06-15 19:14:29 +0200

    sm

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: resolve(__dirname, 'index.html'),
+        cv: resolve(__dirname, 'src/cv/cv.html'),
+        projects: resolve(__dirname, 'src/projects/projects.html'),
       },
     },
   },
