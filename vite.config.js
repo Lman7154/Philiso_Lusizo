2063674a212a8461d5336e21047c2c6fe38d0741
Lman7154 <4138912@myuwc.ac.za>
2024-06-15 19:59:09 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         cv: resolve(__dirname, 'src/cv/cv.html'),
-        projects: [resolve(__dirname, 'src/projects/projects.html'), resolve(__dirname, 'src/projects/projects.js')],
+        projects: resolve(__dirname, 'src/projects/projects.html'),
       },
     },
   },
@@ -18,5 +18,3 @@ export default defineConfig({
 //For each entry point, we use resolve(__dirname, 'path/to/file.html') to specify the path to the respective HTML file relative to the project root.
 
 //With this configuration, Vite will include all three HTML files (index.html, cv.html, and projects.html) in the build process, resulting in separate bundles for each entry point.
-
-//will also include the scripts of the files
