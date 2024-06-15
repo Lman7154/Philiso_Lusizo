0200903989ecb2365a1c57fe0b1c34f697257bcb
Lman7154 <4138912@myuwc.ac.za>
2024-06-15 20:05:21 +0200

    i was supposed to run this command npm run build for the scrips to be also in the build process so that they can also be deployed

--- vite.config.js
+++ vite.config.js
@@ -18,3 +18,5 @@ export default defineConfig({
 //For each entry point, we use resolve(__dirname, 'path/to/file.html') to specify the path to the respective HTML file relative to the project root.
 
 //With this configuration, Vite will include all three HTML files (index.html, cv.html, and projects.html) in the build process, resulting in separate bundles for each entry point.
+
+//i was supposed to run this command npm run build for the scrips to be also in the build process so that they can also be deployed
