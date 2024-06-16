29bd7e104e9aa8d4709144364748ccbae5e8a7b2
Lman7154 <4138912@myuwc.ac.za>
2024-06-16 17:00:19 +0200

    changing the image path to use the absolute URL of the deployed site.

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -8,7 +8,7 @@ const projects = {
         languages: ["HTML", "CSS", "JavaScript"],
         githubLink: "https://github.com/Lman7154/Philiso_Lusizo",
         liveLink: "https://lusizo.github.io/Portfolio",
-        image: "/public/Images/testing image modal rendering in vercel 2.png",
+        image: "https://philiso-lusizo.vercel.app/public/Images/testing image modal rendering in vercel 3.png",
     },
     2: {
         id: 2,
@@ -17,7 +17,7 @@ const projects = {
         languages: ["HTML", "Tailwind CSS", "JavaScript"],
         githubLink: "https://github.com/Lusizo/Cryptway",
         liveLink: "https://lusizo.github.io/Cryptway",
-        image: "/public/Images/testing image modal rendering in vercel 3.png",
+        image: "https://philiso-lusizo.vercel.app/public/Images/testing image modal rendering in vercel 3.png",
     },
     3: {
         id: 3,
@@ -26,7 +26,7 @@ const projects = {
         languages: ["PHP", "JavaScript"],
         githubLink: "https://github.com/Lusizo/Q2A-Poll-Plugin",
         liveLink: "https://lusizo.github.io/Q2A-Poll-Plugin",
-        image: "/public/Images/testing image modal rendering in vercel.png",
+        image: "https://philiso-lusizo.vercel.app/public/Images/testing image modal rendering in vercel.png",
     }
 };
 
